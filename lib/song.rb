5a1774d60f997c059a398ac41e650fa8ab205d5b
Arnold Su <arnoldtingsu@gmail.com>
2018-12-20 06:34:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,16 @@ def self.count
  def self.artists
    @@artists.uniq
  end
+ 
+ def self.artist_count
+   artist_count = {}
+   @@artists.each do |artist|
+     if artist_count[artist]
+       artist_count[artist] += 1
+     else
+       artist_count[artist] = 1
+     end
+   end
 
  def self.genres
    @@genres.uniq
