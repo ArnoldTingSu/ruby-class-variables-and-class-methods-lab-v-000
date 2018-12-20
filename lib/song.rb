563bec9edf261ab5b56fff063075867db74eed28
Arnold Su <arnoldtingsu@gmail.com>
2018-12-20 06:27:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ def self.count
    genre_count = {}
    @@genres.each do |genre|
      if genres_count[genre]
-       genre_count[genre] =+ 1
+       genre_count[genre] += 1
      else
        genre_count[genre] = 1
      end
